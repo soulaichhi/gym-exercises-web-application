30a72633d22224dab776cd6b1de7417fc0fc065c
unknown <chhibou.soulaimane@gmail.com>
2022-07-07 14:33:28 +0200

    FIle & Folder structuring DONE

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width='400px' sx={{ width: {xl: '1488px'}}} m='auto'>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/exercise/:id' element={<ExerciseDetail />}/>
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
