52834ce9162fee8d338042218d24644ca95cf102
unknown <chhibou.soulaimane@gmail.com>
2022-07-09 17:14:15 +0200

    Complete Exercises Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,10 +14,10 @@ const Home = () => {
         bodyPart={bodyPart} 
         setBodyPart={setBodyPart}
       />
-      <Exercises 
+      <Exercises
+        exercises={exercises} 
         setExercises={setExercises}
         bodyPart={bodyPart} 
-        setBodyPart={setBodyPart}
       />
     </Box>
   )
